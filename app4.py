996ba2e7b49310130d23b4e8a6caf52454550ed2
三浦文人 <miurafumito@miurafumitonoMacBook-Air.local>
2024-09-23 19:47:54 +0900

    complete app4.py

--- app4.py
+++ app4.py
@@ -1,2 +1,17 @@
 import openpyxl
-from openpyxl.styles import PattternFill
+from openpyxl.styles import PatternFill
+
+# Excelファイルをロード
+wb = openpyxl.load_workbook("files/製造マスタサンプル.xlsx")
+
+# アクティブなシートを選択
+ws = wb.active
+
+# B列、E列、J列のすべてのセルの値をリストに保存
+b_column_values = []
+e_column_values = []
+j_column_values = []
+
+# B列の値を取得
+for cell in ws["B"]:
+    b_column_values.append(cell.value)
